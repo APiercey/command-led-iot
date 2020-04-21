ef829267eb8367e6cbb083fe16d53453e54244f4
Alexander Butt-Piercey <alexander.butt-piercey@solarisbank.de>
2020-04-21 21:24:54 +0000

    Debugging

--- command-bridge/source/lib/myhome/set_luminosity.ex
+++ command-bridge/source/lib/myhome/set_luminosity.ex
@@ -1,6 +1,6 @@
 defmodule Myhome.SetLuminosity do
   def call(luminosity) do
-    :ok = Tortoise.publish("myhome", "command", "luminosity=#{luminosity}")
+    :ok = Tortoise.publish("myhome", "command", Jason.encode!(%{"luminosity" => luminosity}))
     {:ok, "Set"}
   end
 end
